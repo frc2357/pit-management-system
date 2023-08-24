2f91e63de73fb5ca1cb5485040dfdbbae3a1218f
MidgleyM <115593302+MidgleyM@users.noreply.github.com>
2023-08-24 17:41:41 -0500

    the C# side of the bluetooth

--- Pit Managment System/Bluetooth Downloader/MainWindow.xaml.cs
+++ Pit Managment System/Bluetooth Downloader/MainWindow.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using InTheHand.Net;
+using System.IO;
 using InTheHand.Net.Bluetooth;
 using InTheHand.Net.Sockets;
 using System.Linq;
@@ -15,6 +16,8 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Diagnostics;
+using System.Windows.Controls.Primitives;
 
 namespace Bluetooth_Downloader
 {
@@ -23,54 +26,54 @@ namespace Bluetooth_Downloader
     /// </summary>
     public partial class MainWindow : Window
     {
+        public BluetoothClient workingClient;
         public MainWindow()
         {
             InitializeComponent();
             DiscoverDevices();
         }
-        public static readonly Guid FtpProtocol;
-        public static BluetoothClient client;
-        public static BluetoothListener listener;
-        static void DiscoverDevices()
+        public  readonly Guid FtpProtocol;
+        public  BluetoothClient client;
+        public  BluetoothListener listener;
+         void DiscoverDevices()
         {
             client = new BluetoothClient();
             listener = new BluetoothListener(BluetoothService.SerialPort);
             
-            string mac = "9C:5F:B0:17:18:2F";
+            string phonemac = "9C:5F:B0:17:18:2F";
+            Debug.WriteLine("made it 1");
+            string mac = "b8:5f:98:f0:56:57";
             string Nmac = "4C:79:75:DA:83:42";
             string hex = mac.Replace(":", "");
             ulong arr = Convert.ToUInt64(hex, 16);
+            Debug.WriteLine("made it 2");
             listener.Start();
-            listener.AcceptBluetoothClient();
+            Debug.WriteLine("made it 3");
             BluetoothAddress address = new BluetoothAddress(arr);
-            Console.WriteLine(address.ToString());
-            BluetoothSecurity.PairRequest(address, "");
-            Console.WriteLine("serial port service: "+BluetoothService.SerialPort.ToString());
-            Console.WriteLine("pair request sent");
-            BluetoothEndPoint endPoint = new BluetoothEndPoint(address, BluetoothService.SerialPort, 30);
-            for (int i = 50; i > 0; i--)
-            {
-                if (listener.Pending())
-                {
-                    Console.WriteLine("looking for connection...");
-                    listener.AcceptBluetoothClient();
-                }
-            }
-            IReadOnlyCollection<BluetoothDeviceInfo> devices = client.DiscoverDevices();
-            Console.WriteLine("Discovered Bluetooth Devices: " + devices.ToString());
-            foreach (BluetoothDeviceInfo device in devices)
-            {
-                Console.WriteLine("Name: {0}, Address: {1}", device.DeviceName, device.DeviceAddress);
-            }
-            Console.WriteLine("all connections attempted");
-            client.Close();
-            Console.WriteLine("listener status: "+listener.Active.ToString());
+            Debug.WriteLine(address.ToString()+" ---made it 4");
+            Debug.WriteLine("serial port service: "+BluetoothService.SerialPort.ToString());
+            BluetoothEndPoint endPoint = new BluetoothEndPoint(address, BluetoothService.SerialPort, 21);
+                    Debug.WriteLine("looking for connection...");
+                    workingClient = listener.AcceptBluetoothClient();
+                    Debug.WriteLine("listener has connected to the device");
+           
         }
-        static void pretendFunction(IAsyncResult result) {  
+         void pretendFunction(IAsyncResult result) {  
             if(result.IsCompleted)
             {
-                Console.WriteLine("it finished, is connected: "+client.Connected.ToString());
+                Debug.WriteLine("it finished, is connected: "+client.Connected.ToString());
             }
+        }
+        void writeToStream(object sender, RoutedEventArgs asdjkaFJKEF)
+        {
+            try
+            {
+                var stream = workingClient.GetStream();
+                StreamWriter sw = new StreamWriter(stream, System.Text.Encoding.ASCII);
+                sw.WriteLine("Hello world!");
+                Debug.WriteLine("button pushed, should have wrote");
+            }
+            catch(Exception e) {Debug.WriteLine(e.ToString());}
         }
     }
 }
